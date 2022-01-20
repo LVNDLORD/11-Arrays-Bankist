ce9ccd86c040fb025cfb59b3de73cb52bc2014d6
Andrey <andydeshko@gmail.com>
2022-01-21 00:10:43 +0200

    implemented sorting account movements

--- bankist/script.js
+++ bankist/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) { // better practice to pass data into a function, rather that make func work with global var
+const displayMovements = function (movements, sort = false) { // better practice to pass data into a function, rather that make func work with global var
   containerMovements.innerHTML = ''; // replacing existing html to nothing. Removing prewritten html code
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements; // to sort the copy of the array, without mutating the original one. a-b because we display values from the bottom up 'afterbegin'
+
+  movs.forEach(function (mov, i) {  // using movs instead of movements now it supports sorting option by adding a second paramether in displayMovements
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -187,7 +189,7 @@ btnLoan.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
-})
+});
 
 
 btnClose.addEventListener('click', function (e) {
@@ -203,7 +205,15 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
   labelWelcome.textContent = "Log in to get started";
-})
+});
+
+//sorting transactions
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted); // sorted-true/false is second "sort parameter"
+  sorted = !sorted; // allows to toggle boolean value
+});
 
 
 
